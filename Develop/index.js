b5948d7b66fafd32f4c51c91e007ad205acee768
Kaleb Stafford <kalebstafford0@gmail.com>
2022-05-30 14:33:13 -0500

    packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,17 @@
-// TODO: Include packages needed for this application
+// Allows us to use util functions (needed for writeFileAsync function)
+let util = require("util");
 
+// Allows us to use file system functions (needed for writeFileAsync function)
+let fs = require("fs");
+
+// Convert a method that returns responses using a callback function to return responses in a promise object
+let writeFileAsync = util.promisify(fs.writeFile);
+
+// Allows us to use inquirer (must install first)
+let inquirer = require("inquirer");
+
+// creates generateMarkdown function from generateMarkdown.js in the Utils folder
+let generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [];
 
